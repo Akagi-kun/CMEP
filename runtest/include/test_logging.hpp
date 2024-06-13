36fa522654a5f0a33e29ab580ca6d6bb68f19ed4
Tomáš Slanina <maal-vire@seznam.cz>
2024-06-13 23:32:04 +0200

    Remove runtest (deprecated, unusable)

--- runtest/include/test_logging.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int test_logging_init_release();
-int test_logging_log();
-int test_logging_simplelog();
-int test_logging_map_thread_simplelog();
--- runtest/include/test_vulkan.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int test_vulkan_init_cleanup();
-int test_vulkan_draw_frame();
-int test_vulkan_buffer_create_cleanup();
