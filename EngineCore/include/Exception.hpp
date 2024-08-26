6edc327cc155fac674bb06434597e80ebbc22b65
Tomáš Slanina <maal-vire@seznam.cz>
2024-08-26 03:59:46 +0200

    Fix wrong case used in Engine::Exception

--- EngineCore/include/Exception.hpp
+++ EngineCore/include/Exception.hpp
@@ -5,10 +5,10 @@
 
 namespace Engine
 {
-	class exception : public std::exception
+	class Exception : public std::exception
 	{
 	public:
-		exception(const char* with_file, uint_least32_t with_line, const std::string& with_message)
+		Exception(const char* with_file, uint_least32_t with_line, const std::string& with_message)
 			: message(
 				  std::string(with_file) + " at line " + std::to_string(with_line) + ":\n\t\t" +
 				  with_message
@@ -27,4 +27,4 @@ namespace Engine
 } // namespace Engine
 
 // NOLINTNEXTLINE(*unused-macros)
-#define ENGINE_EXCEPTION(message) ::Engine::exception(__FILE__, __LINE__, message)
+#define ENGINE_EXCEPTION(message) ::Engine::Exception(__FILE__, __LINE__, message)
