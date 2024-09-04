5c72fdf4cf2906e45443b8dfde385cfdaef2d644
Tomáš Slanina <maal-vire@seznam.cz>
2024-09-04 21:07:54 +0200

    - Special handling for llvm if feature test fails - Fix Linux CI - Fix MacOS CI

--- common_include/PlatformSemantics.hpp
+++ common_include/PlatformSemantics.hpp
@@ -2,8 +2,18 @@
 
 #include <version>
 
-#if !defined(__cpp_lib_format)
-#	error "__cpp__lib_format is unsupported!"
+// Checks for std::format support
+#if defined(__cpp_lib_format)
+//
+// Handle the special case of AppleClang not defining __cpp_lib_format when supporting the feature
+// this uses the __has_include extension of clang
+#elif defined(__llvm__) && __has_include(<format>)
+#	pragma message                                                                                \
+		"__cpp_lib_format is not defined but compiler is __llvm__ and __has_include(<format>), treating as compatible"
+//
+// Immediately error out in all other cases
+#else
+#	error "__cpp_lib_format is not supported!"
 #endif
 
 #if defined(_MSC_VER)
